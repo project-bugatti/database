bac9cb66c7c10107f78ef558b02958c2007512ca
Tyler <tgalske@gmail.com>
2019-03-08 19:09:12 -0500

    Added script for media table

--- scripts.sql
+++ scripts.sql
@@ -15,3 +15,15 @@ CREATE TABLE quotes (
   is_visible boolean DEFAULT TRUE,
   FOREIGN KEY (author_member_id) REFERENCES members(member_id)
 );
+
+CREATE TABLE media (
+  media_id uuid NOT NULL PRIMARY KEY,
+  title text NULL,
+  description text NULL,
+  uploaded_at timestamptz NOT NULL DEFAULT now(),
+  media_date date NULL,
+  is_visible boolean NOT NULL DEFAULT TRUE,
+  file_type varchar(10) NULL
+);
+
+
